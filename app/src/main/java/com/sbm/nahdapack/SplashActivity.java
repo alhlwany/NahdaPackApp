d0a023cb88d9d51dbbad4230db520f52284e0a76
Mohamed Alhlwany <moh.abdelmordy.it@gmail.com>
2020-08-27 13:45:50 +0200

    Initial commit

---
+++ app/src/main/java/com/sbm/nahdapack/SplashActivity.java
@@ -0,0 +1,14 @@
+package com.sbm.nahdapack;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
