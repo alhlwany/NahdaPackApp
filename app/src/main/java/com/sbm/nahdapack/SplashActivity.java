1d12e3dce3166cc9df3f1f46ddadec69fe408753
alhlwany <moh.abdelmordy.it@gmail.com>
2020-08-27 18:57:25 +0200

    splash background animation and logo

--- app/src/main/java/com/sbm/nahdapack/SplashActivity.java
+++ app/src/main/java/com/sbm/nahdapack/SplashActivity.java
@@ -1,8 +1,11 @@
 package com.sbm.nahdapack;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        ConstraintLayout constraintLayout = findViewById(R.id.layoutSplash);
+        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2000);
+        animationDrawable.setExitFadeDuration(4000);
+        animationDrawable.start();
     }
 }
